a91a05e51c5628693de5875fcea867e80251235f
is2364 <philipp.dullinger@kit.edu>
2024-11-29 13:02:07 +0100

    inefficient but works

--- identify_atom_types.py
+++ identify_atom_types.py
@@ -141,87 +141,29 @@ def sort_in_brackets(string_name):
                     return final_string
 
 
+#### hier main
+def read_smiles_from_file(file="smiles.txt", header=True):
+    '''reads smiles from file, returns list of smiles'''
+    with open(file, "r") as f:
+        if header:
+            f.readline()
+        return [line.strip() for line in f]
+all_smiles=read_smiles_from_file("smiles.txt")
+all_types=[]
+
 #test with depth 2
-smiles = "CCO"
-atoms, connections = smiles_to_atoms_and_connections(smiles)
-print("Atoms:", atoms)
-print("Connections:", connections)
+for smiles in all_smiles:
+#smiles = "CCO"
+        atoms, connections = smiles_to_atoms_and_connections(smiles)
+        #print("Atoms:", atoms)
+        #print("Connections:", connections)
 
 
 
-a0,a1,a2=identify_from_connection_matrix_depth_2(atoms, connections)
-a2_0_sorted=   [ sort_in_brackets(a2[i]) for i in range(len(a2))]
-print(a2_0_sorted)
-exit()
-def identify_from_connection_matrix_depth_1(atoms, connectivity):
-    atom_types_0={}
-    for i in range(len(atoms)):
-        current_atom=atoms[i]
-        tmp=""
-        tmp+=current_atom+ " ("
-        atom_types_0[i]=atoms[i]
-        for j in range(len(atoms)):
-            if connectivity[j][i]==1 and i!=j:
-                tmp+=atoms[j]
-        tmp+=")"
-        atom_types_0[i]=tmp
-    return atom_types_0    
+        a0,a1,a2=identify_from_connection_matrix_depth_2(atoms, connections)
+        a2_0_sorted=   [ sort_in_brackets(a2[i]) for i in range(len(a2))]
+        #print(a2_0_sorted)
 
-
-def get_connections_via_n(connections,n):
-    '''returns the connectivity matrix along n steps (n>=1) - connection to itself is written as one
-    this trick works because A**n,  paths of length n between two nodes'''
-    for i in range(len(connections)):
-        for j in range(len(connections)):
-            if j==i:
-                connections[i][j]=1
-    
-    M= np.linalg.matrix_power(connections, n)
-    #for i in range(len(M)):
-    #    for j in range(len(M)):
-    #        if M[i][j]>0:
-    #            M[i][j]=1
-    return M
-
-
-print(get_connections_via_n(connections,2
-                            ))
-
-
-def identify_from_connection_matrix_up_to_depth_N(atoms, connectivity, N):
-    '''returns a dictionary with the atom types up to depth N, without previously defined connections'''
-    types_order={}
-    assert isinstance(N, int) and N >= 0, "N must be a positive integer"
-    for i in range(N):
-        types_order[i]={}
-
-    #manually zeroth_order_types    
-    for i in range(len(atoms)):
-        types_order[0][i]=atoms[i]
-    if N==0:
-        return types_order
-    #connections_first order already defined
-    types_order[1]=identify_from_connection_matrix_depth_1(atoms, connectivity)
-    if N==1:
-        return types_order
-    
-
-    
-    #stattdessen lieber connections des vorherigen atoms suchen
-
-    #connections second order and higher
-    #for i in range(2,N+1):
-    #    connections=get_connections_via_n(connectivity,i)
-    #    #connections_one_less=get_connections_via_n(connectivity,i-1)#to subtract so we do not have stuff multiple times
-    #    #connections=connections-connections_one_less
-    #    for j in range(len(atoms)):
-    #        print("Connections for atom",atoms[j],"at depth",i,":")
-    #        for k in range(len(atoms)):
-    #            if connections[j][k]==1:
-    #                print()
-    #                print(atoms[k],end="")
-
-    return types_order
-
-print(identify_from_connection_matrix_up_to_depth_N(atoms, connections, 2))
-
+        all_types.extend(a2_0_sorted)
+print(set(all_types))
+print("Number of different atomtypes identified:", len(set(all_types)))
