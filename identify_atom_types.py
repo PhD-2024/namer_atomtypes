5dc8febdc2f7669bfca74ea02f9d3bb0c62daa59
is2364 <philipp.dullinger@kit.edu>
2024-11-29 12:37:40 +0100

    sort in brackets works correctly for the testcases

--- identify_atom_types.py
+++ identify_atom_types.py
@@ -1,5 +1,6 @@
 from rdkit import Chem
 import numpy as np
+import re
 
 def smiles_to_atoms_and_connections(smiles):
     mol = Chem.MolFromSmiles(smiles)
@@ -16,11 +17,6 @@ def smiles_to_atoms_and_connections(smiles):
     
     return atoms, connections
 
-
-smiles = "CC"
-atoms, connections = smiles_to_atoms_and_connections(smiles)
-print("Atoms:", atoms)
-print("Connections:", connections)
 
 
 
@@ -43,36 +39,119 @@ def identify_from_connection_matrix_depth_2(atoms, connectivity):
         atom_dict_0[i]=zero_string
         atom_dict_1[i]+=atoms[i]+"["
         atom_dict_2[i]+=atoms[i]+"["
-        #print(current_atom)
-        #atom_types_0[i]=atoms[i]
+
         for j in range(len(atoms)):
             
             if connectivity[j][i]==1 and i!=j:
                 
                 atom_dict_1[i]+=atoms[j]
                 atom_dict_2[i]+=atoms[j]+"("
-                #print (atoms[j], "(")
+                
                 for k in range(len(atoms)):
                     if connectivity[k][j]==1 and j!=k:
                         atom_dict_2[i]+=atoms[k]
-                        #print("-------",atoms[k])
+                        
                 atom_dict_2[i]+=")"
-                #print(")")
+                
         atom_dict_1[i]+="]"
         atom_dict_2[i]+="]"    
-        #print("--------------------end")
-            
-                        #tmp+=atoms[k]
-                #tmp+=atoms[j]
-        #tmp+=")"
-    #print(atom_dict_0)
-    #print(atom_dict_1)
-    #print(atom_dict_2)
+
     return atom_dict_0, atom_dict_1, atom_dict_2
 
-#test with depth 2
-print(identify_from_connection_matrix_depth_2(atoms, connections))
+def sort_within_bracket(string_name):
+    '''string to list, then sort list and return as string'''
+    unsorted=list(string_name)
+    #check that the string is neither empty and does not contain chars, that are not letters
+    if len(unsorted)==0:
+        raise ValueError("String is empty")
+    if not all(char.isalpha() for char in unsorted):
+        print(unsorted)
+        raise ValueError("String contains non-alphabetical characters")
 
+    return "".join(sorted(unsorted))
+
+
+
+
+def sort_by_name_and_length(collected):
+    '''sorts the collected list by the atom name and the length of the inner string'''
+    return sorted(collected, key=lambda x: (x[0], x[1]))
+
+def find_parentheses_substrings(string):
+    pattern = re.compile(r'\(.*?\)')
+    return pattern.findall(string)
+
+def sort_in_brackets(string_name):
+    '''splits string into its bracket contributions
+    case 1 is dictionary after one iteration, case 2 
+    is dictionary after two iterations'''
+    #case 1
+    if "[" in string_name: 
+        if "(" not in string_name and ")" not in string_name:
+            if "]"  not in string_name:
+                raise ValueError("Brackets are not balanced")
+            else:
+                if string_name.count("[")!=string_name.count("]"):
+                    raise ValueError("Brackets are not balanced")
+                else:
+                    print("assuming interation 1")
+                    tmp=string_name.replace("]","").split(sep="[")[1:] 
+                    return sort_within_bracket(tmp)
+        #case2        
+        elif "(" in string_name:
+            if ")" not in string_name:
+                raise ValueError("Brackets are not balanced")
+            else:
+                if string_name.count("(")!=string_name.count(")"):
+                    raise ValueError("Brackets are not balanced")
+                else:
+                #first sort the inner brackets "( )"
+                # then they should be equal and we can sort the outer brackets "[ ]"
+                    #find all substrings in parentheses and sort them individually
+                    all_substrings=find_parentheses_substrings(string_name)
+
+
+                    for i in range(len(all_substrings)):
+                        original_substring=all_substrings[i]
+                        #print("to sort", original_substring)
+                        sorted_substring="("+sort_within_bracket(all_substrings[i].replace("(","").replace(")",""))+")"
+                        string_name=string_name.replace(original_substring,sorted_substring)
+                        
+                    #now sort outer brackets
+                    #outer bracket will be sorted alphabetically and if the atoms are the same, the length of the inner string will be compared
+                    
+                    B,working_on=string_name.replace("]","").split(sep="[")
+                    
+                    all_substrings=working_on.split(sep=")")[:-1] # last is empty
+                    
+                    collected=[]
+                    for item in all_substrings:
+                        #print("sub",item)
+                        atom_char, inner=item.split(sep="(")
+                        length=len(inner)
+                        collected.append((atom_char,length, inner))
+                    sorted=sort_by_name_and_length(collected)
+                    stringified=[a[0]+"("+a[2]+")" for a in sorted]
+                    #print(sorted)
+                    #print(stringified)
+
+                    modified_outer="".join(str(x) for x in stringified)
+                                           
+                    final_string=B+"["+modified_outer+"]"
+                    return final_string
+
+
+#test with depth 2
+smiles = "CCO"
+atoms, connections = smiles_to_atoms_and_connections(smiles)
+print("Atoms:", atoms)
+print("Connections:", connections)
+
+
+
+a0,a1,a2=identify_from_connection_matrix_depth_2(atoms, connections)
+a2_0_sorted=   [ sort_in_brackets(a2[i]) for i in range(len(a2))]
+print(a2_0_sorted)
 exit()
 def identify_from_connection_matrix_depth_1(atoms, connectivity):
     atom_types_0={}
